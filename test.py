a90032842503d35dfae5cc9bfbb4398a12337101
Thowfeek <thowfeeksalim002@gmail.com>
2023-08-22 12:30:24 +0530

    last commit

--- test.py
+++ test.py
@@ -1,21 +1,36 @@
 import subprocess
 import json
+import random
 from pymongo import MongoClient
 import os
 
-def mask_sensitive_data(record, sensitive_fields):
-    masked_record = record.copy()
-    for field in sensitive_fields:
-        if field in masked_record:
-            masked_record[field] = '*********'
-    return masked_record
+def generate_fake_data(field):
+    if field == "name":
+        return f"User{random.randint(1000, 9999)}"
+    elif field == "email":
+        return f"user{random.randint(1000, 9999)}@example.com"
+    elif field == "gst_id":
+        return f"{random.randint(100000, 999999)}"
+    elif field == "address":
+        return f"Address{random.randint(1000, 9999)}"
+    elif field == "birthdate":
+        return f"01-01-2000"  
+    elif field == "accounts":
+        return [{"account_id": str(random.randint(1000, 9999))} for _ in range(2)]
+    elif field == "tier_and_details":
+        return {"tier": f"Tier{random.randint(1, 3)}", "details": f"Details{random.randint(1000, 9999)}"}
+    elif field == "active":
+        return random.choice([True, False])
 
 def anonymize_data(input_filename, output_filename, sensitive_fields):
     data = []
     with open(input_filename, 'r') as file:
         for line in file:
             record = json.loads(line)
-            masked_record = mask_sensitive_data(record, sensitive_fields)
+            masked_record = record.copy()
+            for field in sensitive_fields:
+                if field in masked_record:
+                    masked_record[field] = generate_fake_data(field)
             data.append(masked_record)
     
     with open(output_filename, 'w') as file:
@@ -62,7 +77,7 @@ def main():
             ]
             subprocess.run(" ".join(export_command), shell=True)
             
-            # Anonymize data
+            
             anonymize_data(output_file, output_file, sensitive_fields)
             
             # Import data into the destination MongoDB using the same names
